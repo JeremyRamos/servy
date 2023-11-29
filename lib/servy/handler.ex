ee4ba80d6d8f7ff0e845bfc3fd7538ac14148a79
jeremy <jeremyramos92@gmail.com>
2023-11-29 10:58:47 +0200

    play around with enums

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy.Handler do
   import Servy.Parser, only: [parse: 1]
 
   alias Servy.Conv
+  alias Servy.BearController
 
   @doc "Transforms the request into a response"
   def handle(request) do
@@ -24,7 +25,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/bears"} = conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "GET", path: "/bears/new" } = conv) do
@@ -34,16 +35,17 @@ defmodule Servy.Handler do
       |> handle_file(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/bears" <> id} = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  def route(%Conv{ method: "GET", path: "/bears/" <> id} = conv) do
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
-    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
+    BearController.delete(conv, conv.params)
   end
 
   def route(%Conv{ method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}" }
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{method: "GET", path: "/about" } = conv) do
